1ffb85e612acc0c7767024aeb7add01f4a2d446d
duncan1409 <duncansitworld@gmail.com>
2024-05-16 17:28:13 +0900

    [Fix] fix withCredentials setting

--- src/application/json.ts
+++ src/application/json.ts
@@ -3,41 +3,37 @@ import { axios } from "../instance";
 import { Options } from "../interface";
 
 function applicationJson(baseURL = "/api", options?: Options) {
-  const { version, withCredentials } = options ?? {
-    version: "",
-    withCredentials: true,
-  };
+  const { version, withCredentials } = options ?? {};
   const instance = axios(
     "application/json",
     typeof version !== "undefined" ? [baseURL, version].join("/") : baseURL
   );
-  const axiosRequestConfig = withCredentials
-    ? ({ withCredentials: true } as AxiosRequestConfig)
-    : ({} as AxiosRequestConfig);
+
+  const config: AxiosRequestConfig = {
+    withCredentials: withCredentials ?? true,
+  };
 
   return {
     get: <Response = unknown>(url: string, data?: AxiosRequestConfig) => {
       return instance
-        .get<Response>(url, { ...data, ...axiosRequestConfig })
+        .get<Response>(url, { ...data, ...config })
         .then((res) => res.data);
     },
     post: <Request = any, Response = unknown>(url: string, data?: Request) => {
-      return instance
-        .post<Response>(url, data, axiosRequestConfig)
-        .then((res) => res.data);
+      return instance.post<Response>(url, data, config).then((res) => res.data);
     },
     put: <Request = any, Response = unknown>(url: string, data?: Request) => {
-      return instance
-        .put<Response>(url, data, axiosRequestConfig)
-        .then((res) => res.data);
+      return instance.put<Response>(url, data, config).then((res) => res.data);
     },
     patch: <Request = any, Response = unknown>(url: string, data?: Request) => {
       return instance
-        .patch<Response>(url, data, axiosRequestConfig)
+        .patch<Response>(url, data, config)
         .then((res) => res.data);
     },
     delete: <Response = unknown>(url: string, data?: object) => {
-      return instance.delete<Response>(url, data).then((res) => res.data);
+      return instance
+        .delete<Response>(url, { ...data, ...config })
+        .then((res) => res.data);
     },
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import http from "./http";
-import { Http } from "./interface";
+import { Http, Options } from "./interface";
 
-function handler<T extends Record<string, string>>(
-  obj: T
-): { [p in keyof T]: (version?: string) => Http } {
-  return Object.keys(obj).reduce<{
-    [p in keyof T]: (version?: string) => Http;
-  }>((acc, cur) => {
-    acc[cur as keyof T] = (version?: string) => http(obj[cur], { version });
+type Handler<T> = {
+  [p in keyof T]: (
+    version?: string,
+    options?: Omit<Options, "version">
+  ) => Http;
+};
+
+function handler<T extends Record<string, string>>(obj: T): Handler<T> {
+  return Object.keys(obj).reduce<Handler<T>>((acc, cur) => {
+    acc[cur as keyof T] = (
+      version?: string,
+      options?: Omit<Options, "version">
+    ) => http(obj[cur], { version, ...options });
     return acc;
-  }, {} as { [p in keyof T]: (version?: string) => Http });
+  }, {} as Handler<T>);
 }
 
 export { handler };
