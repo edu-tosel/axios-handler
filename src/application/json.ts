fa00c3baa43bf6acced42e4d0430e2ec7fcd6d03
duncan1409 <duncansitworld@gmail.com>
2024-05-23 10:02:31 +0900

    [Feat] add configs

--- src/application/json.ts
+++ src/application/json.ts
@@ -9,30 +9,46 @@ function applicationJson(baseURL = "/api", options?: Options) {
     typeof version !== "undefined" ? [baseURL, version].join("/") : baseURL
   );
 
-  const config: AxiosRequestConfig = {
+  const defaultConfig: AxiosRequestConfig = {
     withCredentials: withCredentials ?? true,
   };
 
   return {
     get: <Response = unknown>(url: string, data?: AxiosRequestConfig) => {
       return instance
-        .get<Response>(url, { ...data, ...config })
+        .get<Response>(url, { ...data, ...defaultConfig })
         .then((res) => res.data);
     },
-    post: <Request = any, Response = unknown>(url: string, data?: Request) => {
-      return instance.post<Response>(url, data, config).then((res) => res.data);
-    },
-    put: <Request = any, Response = unknown>(url: string, data?: Request) => {
-      return instance.put<Response>(url, data, config).then((res) => res.data);
-    },
-    patch: <Request = any, Response = unknown>(url: string, data?: Request) => {
+    post: <Request = any, Response = unknown>(
+      url: string,
+      data?: Request,
+      config?: AxiosRequestConfig
+    ) => {
       return instance
-        .patch<Response>(url, data, config)
+        .post<Response>(url, data, { ...defaultConfig, ...config })
+        .then((res) => res.data);
+    },
+    put: <Request = any, Response = unknown>(
+      url: string,
+      data?: Request,
+      config?: AxiosRequestConfig
+    ) => {
+      return instance
+        .put<Response>(url, data, { ...defaultConfig, ...config })
+        .then((res) => res.data);
+    },
+    patch: <Request = any, Response = unknown>(
+      url: string,
+      data?: Request,
+      config?: AxiosRequestConfig
+    ) => {
+      return instance
+        .patch<Response>(url, data, { ...defaultConfig, ...config })
         .then((res) => res.data);
     },
     delete: <Response = unknown>(url: string, data?: object) => {
       return instance
-        .delete<Response>(url, { ...data, ...config })
+        .delete<Response>(url, { ...data, ...defaultConfig })
         .then((res) => res.data);
     },
   };
