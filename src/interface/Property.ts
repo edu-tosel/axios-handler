58729d4e7cded5d2e210b57790399e7aec73c241
minhyeong <minhyeong.dev@gmail.com>
2024-11-24 23:36:06 +0900

    [Feat] add xml data type

--- src/interface/Property.ts
+++ src/interface/Property.ts
@@ -1,9 +1,10 @@
 type DataType =
   | "application/json"
   | "application/x-www-form-urlencoded"
+  | "application/xml"
+  | "application/octet-stream"
   | "multipart/form-data"
-  | "text/html"
-  | "application/octet-stream";
+  | "text/html";
 
 interface Options {
   version?: string;
